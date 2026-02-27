a9261e3c58d55345b029273d5d631ba65d99cd69
raya <qarraya997@gmail.com>
2026-02-27 13:43:54 +0200

    deploy the project

--- controllers/diseases/Diseases.Routes.js
+++ controllers/diseases/Diseases.Routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createChronicDisease, deleteChronicDisease, getChronicDiseaseById, updateChronicDisease, } from "./Diseases.Controller.js";
+import { createChronicDisease, deleteChronicDisease, getAllChronicDiseases, getChronicDiseaseById, updateChronicDisease, } from "./Diseases.Controller.js";
 
 const router = express.Router();
 
 router.post("/", createChronicDisease);
+router.get("/", getAllChronicDiseases); // Assuming this is the route to get all chronic diseases
 router.get("/:id", getChronicDiseaseById);
 router.put("/:id", updateChronicDisease);
 router.delete("/:id", deleteChronicDisease);
