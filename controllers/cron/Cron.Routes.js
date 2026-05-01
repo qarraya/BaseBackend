2d944f9e9b6617bf3630370ce0c6f84136dbce6f
raya <qarraya997@gmail.com>
2026-05-01 16:00:23 +0300

    Revert "Add Vercel native Cron support and refactor cron jobs for serverless"
    This reverts commit c28e335a77e44379a217d7a52ff5393be48ee1b0.

--- controllers/cron/Cron.Routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { triggerCron } from "./Cron.Controller.js";
-
-const router = express.Router();
-
-// Public route but protected by x-vercel-cron header or Bearer secret
-router.get("/trigger", triggerCron);
-
-export default router;
