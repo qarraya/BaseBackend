f7209e4b3112b919276e66525c85f9c82f0e8e4a
raya <qarraya997@gmail.com>
2026-05-01 17:54:51 +0300

    Fix: Prioritize Authorization header in verifyAdmin

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -4,14 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const verifyAdmin = (req, res, next) => {
-  let token = req.cookies?.auth_token;
+  let token = null;
 
-  if (!token && req.headers.authorization) {
+  // 1. Check Authorization Header FIRST (to prioritize manual testing)
+  if (req.headers.authorization) {
     if (req.headers.authorization.startsWith("Bearer ")) {
       token = req.headers.authorization.split(" ")[1];
     } else {
       token = req.headers.authorization;
     }
+  }
+
+  // 2. If no header, check cookies
+  if (!token && req.cookies?.auth_token) {
+    token = req.cookies.auth_token;
   }
 
   if (!token) {
